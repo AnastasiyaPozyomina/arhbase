e06d641ac985995d2056d662c5e6130fe5479d57
AnastasiyaPozyomina <pozyomina@gmail.com>
2024-06-09 12:33:32 +0700

    setup gh-pages

--- arhbase/docusaurus.config.js
+++ arhbase/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'AnastasiyaPozyomina', // Usually your GitHub org/user name.
+  organizationName: 'AnastasiyaPozyomina.github.io', // Usually your GitHub org/user name.
   projectName: 'arhbase', // Usually your repo name.
 
   onBrokenLinks: 'throw',
