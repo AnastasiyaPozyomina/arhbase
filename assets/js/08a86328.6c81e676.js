054fa123ca6d1522119fa4660b2de2790a5f2bdd
AnastasiyaPozyomina <pozyomina@gmail.com>
2025-06-03 23:27:48 +0700

    Updates

--- assets/js/08a86328.6c81e676.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkarhbase=self.webpackChunkarhbase||[]).push([[351],{8576:(e,s,a)=>{a.r(s),a.d(s,{default:()=>r});a(6540);var c=a(9795),h=a(4848);const r=()=>(0,h.jsx)(c.A,{children:(0,h.jsx)("main",{children:(0,h.jsx)("h1",{className:"center",children:"\u0426\u0435\u0440\u043a\u0432\u0438"})})})}}]);
---
+++ assets/js/08a86328.986ed023.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkarhbase=self.webpackChunkarhbase||[]).push([[351],{4501:(e,s,a)=>{a.r(s),a.d(s,{default:()=>r});a(6540);var c=a(6250),h=a(4848);const r=()=>(0,h.jsx)(c.A,{children:(0,h.jsx)("main",{children:(0,h.jsx)("h1",{className:"center",children:"\u0426\u0435\u0440\u043a\u0432\u0438"})})})}}]);
